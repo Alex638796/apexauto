4492bc56d6e295a7cd33ab1b92c9f53703835102
Alex638796 <alex638796@gmail.com>
2026-09-24 21:55:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
-##Code Written By @ItsMeMaster
-##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M"
-    DB_NAME = "nattu"
-    API_ID = "6886135"
-    API_HASH = "ee20a1c8a8e44eaa638b7254cbcc3012"
-    ADMIN_ID = 8532700793
-    DB_URL = "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0"
-    LOG_CHANNEL = "-1003873871474" # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = "https://t.me/Natking_uploader_bot"
-    TUTORIAL_VIDEO = "https://t.me/Natking_uploader_bot"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH", "")
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "AppxBot")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "0")
+    USERLINK = os.environ.get("USERLINK", "https://t.me/")
+    TUTORIAL_VIDEO = os.environ.get("TUTORIAL_VIDEO", "https://t.me/")
